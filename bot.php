3ea68b65009313781b837a7ce61ca40587f01d36
akebundit <bunditake@gmail.com>
2017-11-09 15:54:22 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -10,7 +10,7 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text' && substr($event['message']['text'],0,5) == 'sthome' ) {
 			// Get text sent
 			$text = $event['message']['text'];
 			// Get replyToken
