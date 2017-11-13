7833635d6dbe5a2ea3bfe1fb3dc6ce98c5aff73d
akebundit <bunditake@gmail.com>
2017-11-13 16:43:42 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -10,7 +10,7 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text' && substr($event['message']['text'],0,4) == 'lamo' ) {
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text' ) {
 			$aa = strpos($event['message']['text'],' ');
 			$bb  = strlen($event['message']['text']);	
 			$an = substr($event['message']['text'],5 , $bb );
